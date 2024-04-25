7c0a2525a883394f4ab0fc6dca8716a959bd0554
byoungyoon <bur5698@naver.com>
2024-04-25 17:14:21 +0900

    :fire: css 오류 수정 및 오타 수정

--- src/app/(beforeLogin)/_component/checkBoxGroupTheme.css.ts
+++ src/app/(beforeLogin)/_component/checkBoxGroupTheme.css.ts
@@ -1,6 +1,6 @@
 import { style } from '@vanilla-extract/css';
 import * as Theme from '@/app/theme.css';
-import { responsiveStyle } from '@/app/theme.css';
+import { colors, responsiveStyle } from '@/app/theme.css';
 
 export const container = style({});
 
@@ -19,12 +19,23 @@ export const layerReadonly = style({
 
 export const layerInput = style([
   Theme.text.subTitle,
-  Theme.customInput,
+  responsiveStyle({
+    tablet: {
+      padding: '12px 54px',
+    },
+    desktop: {
+      padding: '20px 54px',
+    },
+  }),
   {
     paddingLeft: '54px',
     pointerEvents: 'none',
     color: Theme.colors['gray-60'],
     borderRadius: 0,
+    outline: 'none',
+    width: '100%',
+    border: '1px solid',
+    borderColor: colors['gray-40'],
     selectors: {
       'div:first-child > &': {
         borderRadius: '10px 10px 0 0',
